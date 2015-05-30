a0e014186e68e0abdfdf6faabdde1c0e5f3690be
lukaszzbl <lukaszzbl@wp.pl>
2015-05-30 19:39:32 +0200

    Dodanie projektu

---
+++ Pr_III/Pr_III.cpp
@@ -0,0 +1,16 @@
+// Pr_III.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <gdiplus.h>
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	system("PAUSE");
+	return 0;
+}
+
---
+++ Pr_III/stdafx.cpp
@@ -0,0 +1,11 @@
+// stdafx.cpp: plik Ÿród³owy , który zawiera tylko standardowe zawarcia
+// Pr_III.pch bêdzie wstêpnie skompilowanym nag³ówkiem
+// stdafx.obj bêdzie zawieraæ informacje typu wstêpnie skompilowanego
+
+#include <iostream>
+#include <windows.h>
+#include <gdiplus.h>
+#include "stdafx.h"
+
+// TODO: odwo³aj siê do dowolnych dodatkowych nag³ówków, których potrzebujesz w pliku STDAFX.H
+// a nie w tym pliku
